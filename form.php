82fc44523cae674c859f23bd85b11126f528846e
s0169469 <113235284+s0169469@users.noreply.github.com>
2023-05-15 13:52:20 +0300

    Update form.php

--- form.php
+++ form.php
@@ -1,67 +1,47 @@
 <html lang="ru">
 <head>
-    <title>Task3</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="style.css" type="text/css">
+        <link rel="stylesheet" href="style.css">
+</head> 
+<form action="" class="forma" method="POST">
+    <label>
+        Имя:<br>
+        <input id="data" name="name" placeholder="Введите Ваше имя">
+    </label><br>
+    <label>
+        Email:<br>
+        <input id="data" name="email" type="email" placeholder="Введите вашу почту">
+    </label><br>
+    <label>
+        Дата рождения:<br>
+         <select id="data" name="birth_date">
+         <?php 
+          for ($i = 1922; $i <= 2022; $i++) {
+          printf('<option value="%d">%d год</option>', $i, $i);
+          }
+         ?>
+        </select>  
+    </label><br>
+    Пол:<br>
+      <label><input id="data" type="radio" name="sex" value="ж">Ж</label>
+      <label><input id="data" type="radio" name="sex" value="м">М</label><br />
+    Количество конечностей:<br />
+      <label><input id="data" type="radio" name="amount_of_limbs" value="2"> 2 </label>
+      <label><input id="data" type="radio" name="amount_of_limbs" value="3"> 3 </label>
+      <label><input id="data" type="radio" name="amount_of_limbs" value="4"> 4 </label><br>
+    <label>
+        Сверхспособности:<br>
+        <select id="data" name="abilities[]" multiple="multiple">
+          <option value="Бессмертие">Бессмертие</option>
+          <option value="Прохождение сквозь стены">Прохождение сквозь стены</option>
+          <option value="Левитация">Левитация</option>
+        </select>
+    </label><br>
+    <label>
+        Биография:<br />
+        <textarea id="data" name="biography" placeholder="Введите текст"></textarea>
+    </label><br>
+    <label><input id="data" type="checkbox" name="informed">С контрактом ознакомлен(а)</label><br>
+    <input id="sub" type="submit" value="Отправить">
+  </form>
     
-</head>
-<body>
-    <?php 
-      $stmt = $db->prepare("SELECT * FROM ability");
-      $stmt->execute();
-      $result = $stmt->fetchAll();
-    ?>
-    <div class="content">
-    <h1><a id="forma"></a>Форма:</h1>
-      <form action="index.php" method="POST">
-        <label>Ваше имя:<input type="text" name="name"placeholder="Введите имя"/></label><br/>
-        <label>Почта <input type="email" name="email" placeholder="Введите почту"/></label><br/>
-        <label>Дата рождения:<input type="date" name="date" value="2003-03-03"/></label><br/>
-        <p>
-          <label> Пол:
-            <input type="radio" checked="checked" name="gender" value="Female"/>Женский</label>
-          <label><input type="radio" name="gender" value="Male" />Мужской</label><br />
-        </p>
-        <p>
-          Kоличество конечностей:<br/>
-          <label><input type="radio" checked="checked" name="limbs" value="2"/>2</label>
-          <label><input type="radio" name="limbs" value="4"/>4</label>
-          <label><input type="radio" name="limbs" value="6"/>6</label>
-          <label><input type="radio" name="limbs" value="8"/>8</label><br/>
-        </p>
-        <p>
-        <label>
-          Cверхспособности:<br />
-          <select name="abilities[]" multiple="multiple">
-            <?php foreach($result as $res) { ?>
-              <option value="<?php echo $res['AbId']; ?>"><?php echo $res['AbName']; ?></option>
-            <?php } ?>
-          </select>
-        </label><br />
-        </p>
-        <p>
-        <label>Биография:<br/>
-          <textarea name="biog"></textarea>
-        </label><br />
-        </p>
-        <a id="bottom"></a><br/>
-        <label><input type="checkbox" name="check" />С контрактом ознакомлен(а)</label><br/>
-        <p><input type="submit" name="send" value="Отправить"/></p>
-      </form>
-    </div>
-    <ol>
-		<li>
-			<h4>Вход в MariaBD</h4>
-			<img src="SignIn.PNG">
-		</li>
-        <li>
-			<h4>Описание таблиц</h4>
-			<img src="DescribeTable.PNG">
-		</li>
-        <li>
-			<h4>Содержание таблиц</h4>
-			<img src="SelectTable.PNG">
-		</li>
-    </ol>
-</body>
 </html>
